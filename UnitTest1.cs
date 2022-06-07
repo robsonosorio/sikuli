9ab37a0ba073c0157a7e86853041f8ce843ad562
robson.junior <robson.junior@guarida.com.br>
2022-06-07 14:36:21 -0300

    init

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sikuli
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
